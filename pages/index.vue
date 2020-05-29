5951da80182d3f1365616f41b683ba517eddb399
Andrey Gaisinskii <andygaisinskii@gmail.com>
2020-05-29 18:46:31 +0300

    initial commit and first five cases added to cases.json

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ cases }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import cases from '@/cases/cases.json'
+
+export default Vue.extend({
+  components: {},
+  data() {
+    return {
+      cases
+    }
+  }
+})
+</script>
+
+<style></style>
